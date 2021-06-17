4daf0232e234915eb370ab67c82e591b7a79e46b
dunicn <nemanjazdunic@gmail.com>
2021-06-17 15:53:03 +0200

    added database connection

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host: 'postgresql-lively-03553',
-    user: 'postgres',
-    password: '1234',
-    database: 'smartbrain',
+    host: process.env.DATABASE_URL,
+    ssl: true,
   },
 });
 
